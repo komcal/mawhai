963615c646a2bd38ccd5ae2b74c30ad9c478483f
komcal <kom.sujautra@gmail.com>
2016-11-25 10:29:19 +0700

    view log event

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
+      console.log(event.message)
       sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
       sendTextMessage(sender, 'meow meow~*')
     }
