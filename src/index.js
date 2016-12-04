de77a7bc1a8eaff0f0ff4bbde028e07c7ded4a73
tidjungs <waewprach.s@ku.th>
2016-12-04 21:32:13 +0700

    use routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import routes from './routes'
+
 const app = express()
 
 // parse application/x-www-form-urlencoded
@@ -9,6 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 // parse application/json
 app.use(bodyParser.json())
+
+app.use('/', routes)
 
 // spin spin sugar
 const port = process.env.PORT || 5000
