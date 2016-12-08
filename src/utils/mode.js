72218e776c85d7b6f3b25e351cd742dc755a77cf
tidjungs <waewprach.s@ku.th>
2016-12-08 18:40:20 +0700

    log changeMode

--- src/utils/mode.js
+++ src/utils/mode.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 
 export const changeMode = (mode) => {
+  console.log(mode)
   fs.writeFile('./mode.txt', JSON.stringify({ 'mode': mode }), (err) => {
     if (err) {
       console.log('Error')
