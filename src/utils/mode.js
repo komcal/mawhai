05f0f9bbbf6ca9041483ad7105f4d69699b7c61a
tidjungs <waewprach.s@ku.th>
2016-12-08 18:37:53 +0700

    add json.stringify to changeMode

--- src/utils/mode.js
+++ src/utils/mode.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export const changeMode = (mode) => {
-  fs.writeFile('./mode.txt', { 'mode': mode }, (err) => {
+  fs.writeFile('./mode.txt', JSON.stringify({ 'mode': mode }), (err) => {
     if (err) {
       console.log('Error')
     } else {
